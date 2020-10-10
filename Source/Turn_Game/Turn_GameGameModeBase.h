53346b82546f5aca81e19064081c9c2a11073e3c
JH <wlghks1502@gmailc.om>
2020-10-10 17:23:46 +0900

    초기 커밋 메시지입니다.

---
+++ Source/Turn_Game/Turn_GameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Turn_GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURN_GAME_API ATurn_GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
